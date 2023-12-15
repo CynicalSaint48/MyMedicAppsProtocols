005308077982d5513136605f45a6f50edce96b27
Michael Schmidt <micahel@ninersabroad.com>
2023-12-15 23:27:23 +0000

    Registration/Login Need dB

--- master/users/routes.py
+++ master/users/routes.py
@@ -11,12 +11,20 @@ def register():
     form = RegistrationForm()
 
     if form.validate_on_submit():
-        flash(f'Account Created for {form.username.data}!', 'message_success')
+        flash(f'Account Created for {form.email.data}!', 'message_success')
         return redirect(url_for('main.home'))
     
     return render_template('register.html', varTitle='Register', form=form)
 
-@users.route("/login")
+@users.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'michaels3@medic911.com' and form.password.data == 'password':
+            return redirect(url_for('main.home'))
+        else:
+            flash('Login Unsuccessful.  Please check your email and password', 'danger')
+            return redirect(url_for('users.login'))
+
+    print('We skipped a bunch of stuff')
     return render_template('login.html', varTitle='Login', form=form)
