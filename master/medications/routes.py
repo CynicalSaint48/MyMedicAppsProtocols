4a86b21457d07743a85604a42ca46fbf10acd701
Michael Schmidt <micahel@ninersabroad.com>
2023-12-20 23:37:56 +0000

    Dilt and Dex done

--- master/medications/routes.py
+++ master/medications/routes.py
@@ -4,19 +4,15 @@ from flask import session
 medications = Blueprint('medications', __name__)
 
 @medications.route("/medications/dexamethasone")
-def dexamethasone():
-
-    
+def dexamethasone():    
     if not session['ptYears']:
         ptYears = 0
         ptKgs = 0.0
     else:
         ptYears = session['ptYears']
         ptKgs = session['ptKgs']
-
     ptAdult = False
     ptMax26 = False
-
     if ptYears >= 18:
         ptAdult = True
         ptMax26 = False
@@ -28,5 +24,20 @@ def dexamethasone():
         else:
             ptMax26 = False
 
-
     return render_template('/medications/dexamethasone.html', ptAdult=ptAdult, ptMax26=ptMax26, ptYears=ptYears, ptKgs=ptKgs)
+
+@medications.route("/medications/diltiazem")
+def diltiazem():    
+    if not session['ptYears']:
+        ptYears = 0
+        ptKgs = 0.0
+    else:
+        ptYears = session['ptYears']
+        ptKgs = session['ptKgs']
+    ptAdult = False
+    if ptYears >= 18:
+        ptAdult = True
+    else:
+        ptAdult = False
+
+    return render_template('/medications/diltiazem.html', ptAdult=ptAdult, ptYears=ptYears, ptKgs=ptKgs)
