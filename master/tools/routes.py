e8acf30b8342e48bc504ee08e95587911f7298c8
Michael Schmidt <micahel@ninersabroad.com>
2024-01-23 23:04:39 +0000

    added ALS upgrade Tool

--- master/tools/routes.py
+++ master/tools/routes.py
@@ -14,3 +14,10 @@ def phoneNumbers():
     session['path'] = 'main.home'
     return render_template('/tools/PhoneNumbers.html', keys=keys, varTitle="Useful Phone Numbers")
 
+@tools.route("/tools/BLS911Upgrade")
+def alsUpgrade():
+
+    keys = get_key_list()
+    session['path'] = 'tools.alsUpgrade'
+    return render_template('/tools/BLS911Upgrade.html', keys=keys, varTitle="BLS 911 ALS Upgrade Tool")
+
